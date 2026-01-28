64aec581d7f9356e8e79e9c2904069ffb731969f
chauhaneishika <chauhaneishika@gmail.com>
2026-01-28 21:23:58 +0530

    Added HashMap programs..

--- HashMap Java/src/HashSet1.java
+++ HashMap Java/src/HashSet1.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class HashSet1 {
     static void main() {
@@ -12,12 +13,31 @@ public class HashSet1 {
         set.add(3);
         set.add(1); //list.add(el)
 
+        //Size
+        System.out.println("Size of set is : " + set.size());
+
+        //Print all elements
+        System.out.println(set);
+
+        //Iterator
+        Iterator it = set.iterator();
+        //hasNext; next
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+
         //Search - contains
         if(set.contains(1)) {
             System.out.println("Set Contains 1");
         }
         if(!set.contains(6)) {
             System.out.println("Does not contain");
+        }
+
+        //Delete
+        set.remove(1);
+        if(!set.contains(1)) {
+            System.out.println("Does not contain 1 - we deleted 1");
         }
     }
 }
