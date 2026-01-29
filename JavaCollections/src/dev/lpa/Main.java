cff8295c8eb04abf293d8ec8138b6336d8758ffc
chauhaneishika <chauhaneishika@gmail.com>
2026-01-29 14:28:48 +0530

    Added Advance Java programs..

--- JavaCollections/src/dev/lpa/Main.java
+++ JavaCollections/src/dev/lpa/Main.java
@@ -1,11 +1,17 @@
 //ArrayList
 package dev.lpa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 record GroceryItem(String name, String type, int count){
     public GroceryItem(String name ) {
         this(name, "DIARY", 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s",count,name.toUpperCase(),type);
     }
 }
 
@@ -16,5 +22,18 @@ public class Main{
         groceryArray[1] = new GroceryItem("Apples","PRODUCE",6);
         groceryArray[2] = new GroceryItem("oranges","PRODUCE",5);
         System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(0,
+                new GroceryItem("oranges","PRODUCE",5));
+        groceryList.remove(1);
+        System.out.println(groceryList);
+
     }
 }
