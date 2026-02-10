b80db2762a8c9ba0ce4962d305e6c009f2a57d00
Eishika Chauhan <chauhaneishika@gmail.com>
2026-02-10 23:23:01 +0530

    Important questions of Linked Lists

--- LinkedLists/LinkedLists/LLInterviewQ.java
+++ LinkedLists/LinkedLists/LLInterviewQ.java
@@ -1,5 +1,7 @@
-package LinkedLists;
+//package LinkedLists;
 
 public class LLInterviewQ {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
