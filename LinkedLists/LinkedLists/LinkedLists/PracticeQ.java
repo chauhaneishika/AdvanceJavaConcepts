99cb76107dae763f764540a301d595e9cc90f69c
Eishika Chauhan <chauhaneishika@gmail.com>
2026-02-16 20:07:23 +0530

    LinkedLists hw q

--- LinkedLists/LinkedLists/LinkedLists/PracticeQ.java
+++ LinkedLists/LinkedLists/LinkedLists/PracticeQ.java
@@ -1,5 +1,9 @@
+// Given a linked list, split it into two lists such that one contains
+// odd values, while the other contains even values.
 package LinkedLists;
 
 public class PracticeQ {
     
 }
+// Remove dupicates from a sorted Linked List
+
