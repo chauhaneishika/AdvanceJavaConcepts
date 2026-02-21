c537c16a924e1fa8fff9f59786be8b785cdf8ca5
Eishika Chauhan <chauhaneishika@gmail.com>
2026-02-21 16:51:50 +0530

    LinkedLists OddEven LinkedLists q

---
+++ LinkedLists/LinkedLists/LinkedLists/OddEvenLL.java
@@ -0,0 +1,40 @@
+package LinkedLists;
+
+public class OddEvenLL {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd = new ListNode(0);
+        ListNode even = new ListNode(0);
+        ListNode tempo = odd;
+        ListNode tempe = even;
+        ListNode temp = head;
+
+        while(temp!= null) {
+            tempo.next = temp;
+            temp = temp.next;
+            tempo = tempo.next;
+
+            tempe.next = temp;
+            if(temp == null) break;
+            temp = temp.next;
+            tempe = tempe.next;
+
+        }
+        odd = odd.next;
+        even = even.next;
+        tempo.next = even;
+        return odd;
+
+    }
+}
