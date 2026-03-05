5348958d1c962040b2413c1592812697c290d58c
Eishika Chauhan <chauhaneishika@gmail.com>
2026-03-05 23:25:26 +0530

    Copy List with random pointer

--- LinkedLists/LinkedLists/LinkedLists/CopyList.java
+++ LinkedLists/LinkedLists/LinkedLists/CopyList.java
@@ -1,3 +1,7 @@
+//Copy List with Random Pointer: Construct a deep copy of linked list where each node contains
+// an additional random pointer which could point to any node in the list or null.
+// Step1: Deep Copy
+// Step2: Connect the list
 package LinkedLists;
 
 public class CopyList {
