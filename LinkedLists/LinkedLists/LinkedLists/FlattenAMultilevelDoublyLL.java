cf521847ad5765068950bfbd8897e6c477bc0643
Eishika Chauhan <chauhaneishika@gmail.com>
2026-03-16 21:29:16 +0530

    Flatten a MultiLevel Double Linked Lists

--- LinkedLists/LinkedLists/LinkedLists/FlattenAMultilevelDoublyLL.java
+++ LinkedLists/LinkedLists/LinkedLists/FlattenAMultilevelDoublyLL.java
@@ -1,5 +1,38 @@
 package LinkedLists;
 
 public class FlattenAMultilevelDoublyLL {
-    
+    public class Node {
+        int val;
+        Node next;
+        Node() {};
+        Node(int val) {
+            this.val = val;
+        }
+        Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public Node flatten(Node head) {
+        Node temp = head;
+        while(temp != null) {
+            Node t = temp.next;
+            if(temp.child != null) {
+                Node c = flatten(temp.child);
+                temp.next = c;
+                c.prev = temp;
+                // c ko aage le jaao
+                while(c.next != null) {
+                    c = c.next;
+                }
+                c.next = t;
+                if(t != null ) t.prev = c;
+            }
+            temp.child = null; // very imp
+            temp = t;
+        }
+        return head;
+    }
+
 }
