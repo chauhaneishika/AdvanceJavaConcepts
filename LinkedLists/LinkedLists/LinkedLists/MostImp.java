94ab8d36c1e8bae41b6cb6b1262a377eb1a33619
Eishika Chauhan <chauhaneishika@gmail.com>
2026-03-21 23:25:45 +0530

    Most Imp q

--- LinkedLists/LinkedLists/LinkedLists/MostImp.java
+++ LinkedLists/LinkedLists/LinkedLists/MostImp.java
@@ -3,8 +3,45 @@
 package LinkedLists;
 
 public class MostImp {
-    
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    public ListNode[] splitOddEven(ListNode head) {
+        ListNode oddDummy = new ListNode(0);
+        ListNode evenDummy = new ListNode(0);
+
+        ListNode oddTail = oddDummy;
+        ListNode evenTail = evenDummy;
+
+        ListNode curr = head;
+
+        while(curr != null) {
+            if(curr.val % 2 == 0) {
+                evenTail.next = curr;
+                evenTail = evenTail.next;
+            } else {
+                oddTail.next = curr;
+                oddTail = oddTail.next;
+            }
+            curr = curr.next;
+        }
+        evenTail.next = null; 
+        oddTail.next = null;
+
+        return new ListNode[]{oddDummy.next, evenDummy.next};
+
+    }
 }
+}
+
 
 
 // Remove dupicates from a sorted Linked List
